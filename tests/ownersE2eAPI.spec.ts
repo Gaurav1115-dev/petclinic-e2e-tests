a3fbd5a26fe4b827a4e67e69f38b5b72fbc1ee22
Gaurav1115-dev <85428863+Gaurav1115-dev@users.noreply.github.com>
2026-09-24 16:13:07 +0530

    fixed CI Build issue

--- tests/ownersE2eAPI.spec.ts
+++ tests/ownersE2eAPI.spec.ts
@@ -2,33 +2,30 @@ import { test, expect } from '@playwright/test';
 import ownersTestData from '../testData/oweners.json';
 import { OwnerApi } from '../APILayer/owenerApi';
 
-// Create an OwnerApi instance and add a new owner using the data from ownersTestData.
-// Fetch the newly created owner by last name and validate that the returned owner details match the created record.
-
 for (const data of ownersTestData) {
+  test(`Add Owner Test Through API- ${data.firstName} ${data.lastName}`, async ({ request }) => {
+    const ownerapi = new OwnerApi(request);
+    const response = await ownerapi.createOwner(data);
+    expect(response.status()).toBe(201);
 
-    test(`Add Owner Test Through API- ${data.firstName} ${data.lastName}`, async ({ request }) => {
+    const createdOwner = await response.json();
 
-        const ownerapi = new OwnerApi(request);
-        const response = await ownerapi.createOwner(data);
-        expect(response.status()).toBe(201);
-        const createdOwner = await response.json();
+    const updatedData = ownerapi.getUpdatedOwnerData(data);
+    const updateResponse = await ownerapi.updateOwner(createdOwner.id, updatedData);
 
-        const updatedData = ownerapi.getUpdatedOwnerData(data);
-        const updateResponse = await ownerapi.updateOwner(createdOwner.id, updatedData);
+    const fetchResponse = await ownerapi.fetchOwnerByLastName(data.lastName);
+    expect(fetchResponse.status()).toBe(200);
 
-        const fetchResponse = await ownerapi.fetchOwnerByLastName(data.lastName);
-        expect(fetchResponse.status()).toBe(200);
-        const owners = await fetchResponse.json();
-        expect(owners).toContainEqual(
-            expect.objectContaining({
-                id: createdOwner.id,
-                firstName: updatedData.firstName,
-                lastName: updatedData.lastName,
-                address: updatedData.address,
-                city: updatedData.city,
-                telephone: updatedData.telephone,
-            })
-        );
-    })
+    const owners = await fetchResponse.json();
+    expect(owners).toContainEqual(
+      expect.objectContaining({
+        id: createdOwner.id,
+        firstName: updatedData.firstName,
+        lastName: updatedData.lastName,
+        address: updatedData.address,
+        city: updatedData.city,
+        telephone: updatedData.telephone,
+      })
+    );
+  });
 }
